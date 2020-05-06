b8c7e3585c487e73ae681202757ad50dc0f0d8a6
zahuifan <zahuifan@163.com>
2020-05-06 11:17:33 +0800

    初始添加后端项目

---
+++ backend/src/main/java/com/bzb/atjob/AtjobApplication.java
@@ -0,0 +1,13 @@
+package com.bzb.atjob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtjobApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtjobApplication.class, args);
+	}
+
+}
